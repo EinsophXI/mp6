7a89968824e58849f92adbc871e9082a7debad41
Ryan Hwang <hwangr@bu.edu>
2025-12-01 12:39:22 -0500

    update

--- auth.ts
+++ auth.ts
@@ -1,11 +1,8 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const { handlers, auth } = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    })
-  ]
-});
+export const { handlers, auth } = NextAuth(
+  {
+    providers: [GitHub],
+  }
+);
